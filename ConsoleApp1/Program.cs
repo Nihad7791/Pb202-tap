965eca300927932434c39bcd0833a2ae5c2054f8
Nihad7791 <nihadrn@code.edu.az>
2024-10-21 08:18:44 +0400

    second task

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,67 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//int n = 50;
+//int m = 70;
+//int sum = 0;
+//for (int i = n; i <= m; i++)
+//    if (i % 2 == 1)
+//  {
+//      sum += i;
+//  }
+
+//Console.WriteLine(sum);
+//int n = 60;
+//int m = 80;
+//int count = 0;
+//for (int i = n; i<=m; i++)
+//{
+//   if (i%2==1)
+//  {
+//     count++;
+//  }
+//}
+//Console.WriteLine(count);
+//int m = 6;
+//int mult= 1;
+//for (int i = 1; i<=m; i++)
+//{
+//    if (i % 2 == 0)
+//    {
+//       mult *= i;
+//   }
+//}
+//Console.WriteLine(mult);
+using System.ComponentModel.Design;
+
+//int a = 15;
+//if(a%3==0 && a % 2 == 0)
+//{
+//    Console.WriteLine("3-e bolunur ,cutdur");
+//}
+//else if (a%3==0 && a%2==1)
+//{
+//    Console.WriteLine("3-e bolunen,tekdir");
+//}
+//else
+//{
+//    Console.WriteLine("3 bolunmmu");
+
+//}
+//int a = 20;
+//int count = 0;
+//for (int i = 1; i<=25; i++)
+//{
+ //   if (a % i == 0)
+ //   {
+ //       count++;
+  //  }
+  //  if (count > 2)
+  //  { 
+  //      Console.WriteLine("eded murekkebdir");
+  //  }
+  //  else 
+   // {
+   //     Console.WriteLine(" eded sadedir");
+   // }
+
+//}
